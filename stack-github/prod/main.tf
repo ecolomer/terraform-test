f00bc15d826682fe55115ccdf1039f2e364f4feb
Eleatzar Colomer <eleatzar.colomer@glovoapp.com>
2019-11-23 22:59:18 +0100

    Test actions

---
+++ stack-github/prod/main.tf
@@ -0,0 +1,40 @@
+locals {
+  env = "prod"
+  vpc_id = "vpc-38ab525d"
+  custom_tags = {
+    env = local.env
+  }
+}
+
+module "rds-monitor" {
+  source = "../../modules/lambda"
+  aws_managed_policies = [
+    "AWSLambdaVPCAccessExecutionRole"
+  ]
+  project_name = "rds-monitor"
+  function_name = "rds-monitor-liveness"
+  function_source = "lambda"
+  function_runtime = "python3.7"
+  handler_config = { module="main", function="lambda_handler"}
+  custom_tags = merge(local.custom_tags, {project = "rds-monitor"})
+}
+
+#resource "aws_cloudwatch_event_rule" "every_five_minutes" {
+#  name = "every-five-minutes"
+#  description = "Fires every five minutes"
+#  schedule_expression = "rate(5 minutes)"
+#}
+#
+#resource "aws_cloudwatch_event_target" "check_foo_every_five_minutes" {
+#  rule = "${aws_cloudwatch_event_rule.every_five_minutes.name}"
+#  target_id = "check_foo"
+#  arn = "${aws_lambda_function.check_foo.arn}"
+#}
+#
+#resource "aws_lambda_permission" "allow_cloudwatch_to_call_check_foo" {
+#  statement_id = "AllowExecutionFromCloudWatch"
+#  action = "lambda:InvokeFunction"
+#  function_name = "${aws_lambda_function.check_foo.function_name}"
+#  principal = "events.amazonaws.com"
+#  source_arn = "${aws_cloudwatch_event_rule.every_five_minutes.arn}"
+#}
