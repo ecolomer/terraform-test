4f5313a73ffa5dacb3dff649b24f38a1cf38c880
Eleatzar Colomer <eleatzar.colomer@glovoapp.com>
2019-12-08 09:21:26 +0100

    Add events infrastructure

--- stack-test/prod/main.tf
+++ stack-test/prod/main.tf
@@ -2,41 +2,46 @@ locals {
   env = "prod"
   vpc_id = "vpc-2800f04f"
   subnets = [ "subnet-87b14ce0", "subnet-c10e029a" ]
-  custom_tags = {
-    env = local.env
+
+  tags = {
+    project = "rds-monitor"
+    env = "prod"
+    owner = "infrastructure",
+    built-using = "terraform"
   }
 }
 
 module "rds-monitor" {
-  source = "../../modules/lambda"
+  source                 = "app.terraform.io/glovo/lambda/aws"
+  version                = "0.0.9"
   aws_managed_policies = [
     "AWSLambdaVPCAccessExecutionRole"
   ]
   s3_bucket = "ec-lambda-deploy"
-  function_name = "stack-test"
+  function_name = "rds-monitor"
   function_source = "source/"
   function_runtime = "python3.7"
   handler_config = { module="main", function="lambda_handler"}
-  custom_tags = merge(local.custom_tags, {project = "rds-monitor"})
+  custom_tags = local.tags
   vpc_config = { vpc_id = local.vpc_id, subnets = local.subnets }
 }
 
-#resource "aws_cloudwatch_event_rule" "every_five_minutes" {
-#  name = "every-five-minutes"
-#  description = "Fires every five minutes"
-#  schedule_expression = "rate(5 minutes)"
-#}
-#
-#resource "aws_cloudwatch_event_target" "check_foo_every_five_minutes" {
-#  rule = "${aws_cloudwatch_event_rule.every_five_minutes.name}"
-#  target_id = "check_foo"
-#  arn = "${aws_lambda_function.check_foo.arn}"
-#}
-#
-#resource "aws_lambda_permission" "allow_cloudwatch_to_call_check_foo" {
-#  statement_id = "AllowExecutionFromCloudWatch"
-#  action = "lambda:InvokeFunction"
-#  function_name = "${aws_lambda_function.check_foo.function_name}"
-#  principal = "events.amazonaws.com"
-#  source_arn = "${aws_cloudwatch_event_rule.every_five_minutes.arn}"
-#}
+resource "aws_cloudwatch_event_rule" "every_five_minutes" {
+  name = "every-five-minutes"
+  description = "Fires every five minutes"
+  schedule_expression = "rate(5 minutes)"
+}
+
+resource "aws_cloudwatch_event_target" "trigger_lambda" {
+  rule = aws_cloudwatch_event_rule.every_five_minutes.name
+  target_id = "rds-monitor"
+  arn = module.rds-monitor.function_arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_events" {
+  statement_id = "AllowExecutionFromCloudWatch"
+  action = "lambda:InvokeFunction"
+  function_name = "rds-monitor"
+  principal = "events.amazonaws.com"
+  source_arn = aws_cloudwatch_event_rule.every_five_minutes.arn
+}
