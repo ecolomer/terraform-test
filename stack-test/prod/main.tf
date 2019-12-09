2626cbf5fbc8412304e0e1ff70832b46ce421800
Eleatzar Colomer <eleatzar.colomer@glovoapp.com>
2019-12-09 23:20:16 +0100

    Add new infrastructure

--- stack-test/prod/main.tf
+++ stack-test/prod/main.tf
@@ -4,28 +4,67 @@ locals {
   subnets = [ "subnet-87b14ce0", "subnet-c10e029a" ]
 
   tags = {
-    project = "rds-monitor"
+    project = "aurora-monitor"
     env = "prod"
     owner = "infrastructure",
     built-using = "terraform"
   }
 }
 
-module "rds-monitor" {
+resource "random_id" "id" {
+  byte_length = 4
+}
+
+resource "aws_sns_topic" "notify_slack" {
+  name = "notify-slack-${random_id.id.hex}"
+  display_name = "Slack notifications"
+  tags = local.tags
+}
+
+module "notify_slack_lambda" {
   source                 = "../../modules/lambda"
-  aws_managed_policies = [
-    "AWSLambdaVPCAccessExecutionRole"
-  ]
-  custom_policies = [
-    "policies/secrets.json"
-  ]
+
   s3_bucket = "ec-lambda-deploy"
-  function_name = "rds-monitor"
-  function_source = "../source/"
+  function_name = "notify-infrastructure-slack"
+  function_source = "../source/notify-slack"
   function_runtime = "python3.7"
   handler_config = { module="main", function="handler"}
   custom_tags = local.tags
   vpc_config = { vpc_id = local.vpc_id, subnets = local.subnets }
+
+  aws_managed_policies = [
+    "AWSLambdaBasicExecutionRole"
+  ]
+}
+
+resource "aws_lambda_permission" "notify_slack" {
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = module.notify_slack_lambda.function_name
+  principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.notify_slack.arn
+}
+
+module "aurora_monitor_lambda" {
+  source                 = "../../modules/lambda"
+
+  s3_bucket = "ec-lambda-deploy"
+  function_name = "aurora-monitor"
+  function_source = "../source/aurora-monitor"
+  function_runtime = "python3.7"
+  handler_config = { module="main", function="handler"}
+  custom_tags = local.tags
+  vpc_config = { vpc_id = local.vpc_id, subnets = local.subnets }
+
+  aws_managed_policies = [
+    "AWSLambdaVPCAccessExecutionRole"
+  ]
+
+  custom_policies = [
+    "policies/secrets.json"
+  ]
+
+  env_vars = { SNS_SLACK_TOPIC = aws_sns_topic.notify_slack.arn }
 }
 
 resource "aws_cloudwatch_event_rule" "every_five_minutes" {
@@ -36,42 +75,14 @@ resource "aws_cloudwatch_event_rule" "every_five_minutes" {
 
 resource "aws_cloudwatch_event_target" "trigger_lambda" {
   rule = aws_cloudwatch_event_rule.every_five_minutes.name
-  target_id = "rds-monitor"
-  arn = module.rds-monitor.function_arn
+  target_id = module.aurora_monitor_lambda.function_name
+  arn = module.aurora_monitor_lambda.function_arn
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch_events" {
   statement_id = "AllowExecutionFromCloudWatch"
   action = "lambda:InvokeFunction"
-  function_name = "rds-monitor"
+  function_name = module.aurora_monitor_lambda.function_name
   principal = "events.amazonaws.com"
   source_arn = aws_cloudwatch_event_rule.every_five_minutes.arn
-}
-
-resource "aws_sns_topic" "sns_notification" {
-  name = "sns-notification"
-}
-
-data "aws_secretsmanager_secret" "slack_url" {
-  name = "slack-url"
-}
-
-data "aws_secretsmanager_secret_version" "slack_url" {
-  secret_id = data.aws_secretsmanager_secret.slack_url.id
-}
-
-module "notify_slack" {
-  source = "terraform-aws-modules/notify-slack/aws"
-  version = "2.3.0"
-
-  sns_topic_name   = aws_sns_topic.sns_notification.name
-  create_sns_topic = false
-
-  slack_webhook_url = data.aws_secretsmanager_secret_version.slack_url.secret_string
-  slack_channel     = "@eleatzar.colomer"
-  slack_username    = "eleatzar.colomer"
-
-  tags = {
-    Name = "notify-slack-simple"
-  }
 }
---
+++ stack-test/source/notify-slack/main.py
@@ -0,0 +1,34 @@
+import sys
+import os
+import logging
+import json
+import requests
+from requests.exceptions import Timeout
+
+
+def notify_slack(message):
+    slack_url = os.environ['SLACK_WEBHOOK_URL']
+    slack_data = { 'text': message }
+
+    try:
+        response = requests.post(
+            slack_url, data=json.dumps(slack_data),
+            headers={'Content-Type': 'application/json'},
+            Timeout=5
+        )
+    except:
+        raise Exception("ERROR: Could not connect to Slack webhook endpoint")
+
+    if response.status_code != 200:
+        raise ValueError(
+            'Request to slack returned an error %s, the response is:\n%s'
+            % (response.status_code, response.text)
+        )
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def handler(event, context):
+
+    sns_message = json.loads(event['Records'][0]['Sns']['Message'])
+    notify_slack(sns_message['Event Message'])
